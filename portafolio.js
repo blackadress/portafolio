df082134ace3ae7783e6d538a060e88c62f55e46
blackadress <blackadress@github.com>
2019-09-26 21:33:52 -0500

    scroll start

---
+++ portafolio.js
@@ -0,0 +1,11 @@
+let navbar = document.querySelector('nav')
+
+// navbar.children[1].style.display = 'none';
+
+// console.log(navbar.children)
+
+let scrollDownBtn = document.querySelector('#scrollDown')
+
+scrollDownBtn.addEventListener('click', (e) => {
+  // TODO smooth scroll https://www.youtube.com/watch?v=oUSvlrDTLi4
+})
