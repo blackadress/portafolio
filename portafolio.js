44427e8aafad5a98fceb31c5c71f501c8b7a07be
blackadress <blackadress@github.com>
2019-09-26 22:10:37 -0500

    smooth scroll

--- portafolio.js
+++ portafolio.js
@@ -1,11 +1,38 @@
-let navbar = document.querySelector('nav')
+window.onload = () => {
 
-// navbar.children[1].style.display = 'none';
+  let navbar = document.querySelector('nav')
 
-// console.log(navbar.children)
+  let scrollDownBtn = document.querySelector('#scrollDown')
 
-let scrollDownBtn = document.querySelector('#scrollDown')
+  scrollDownBtn.addEventListener('click', (e) => {
+    smoothScroll('#habilidades', 500)
+  })
 
-scrollDownBtn.addEventListener('click', (e) => {
-  // TODO smooth scroll https://www.youtube.com/watch?v=oUSvlrDTLi4
-})
+}
+
+
+const smoothScroll = (target, duration) => {
+  let targetElem = document.querySelector(target)
+  let targetPosition = targetElem.getBoundingClientRect().top
+  let startPosition = window.pageYOffset
+  let distance = targetPosition - startPosition - 150
+  let startTime = null
+  
+  const ease = (t, b, c, d) => {
+    t /= d / 2
+    if (t < 1) return c / 2 * t * t + b
+    t--
+    return - c / 2 * (t * (t - 2) - 1) + b
+  }
+
+  const animation = (currentTime) => {
+    if (startTime === null) startTime = currentTime
+    timeElapsed = currentTime - startTime
+    let run = ease(timeElapsed, startPosition, distance, duration)
+    window.scrollTo(0, run)
+    if(timeElapsed < duration) requestAnimationFrame(animation)
+  }
+
+  requestAnimationFrame(animation)
+
+}
